682b5fad1c187acdb3262e1ab4e10d69dd9b8ac9
Jozef Piecyk <jozef.piecyk@10clouds.com>
2019-09-05 14:02:07 +0200

    fix how static routes are being handled

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -3,21 +3,24 @@ import React, { Component } from 'react';
 
 class Post extends Component {
   static async getInitialProps({ query }) {
-    // pid = 'hello-nextjs'
     const { slug } = query;
     const blogpost = await import(`../../content/blogPosts/${slug}.md`).catch(error => null);
 
     return { blogpost };
   }
   render() {
-    console.log('leci cokolwiek?');
-    //   empty post
     if (!this.props.blogpost) return <div>not found</div>;
-    // const router = useRouter();
-    const { html, title } = this.props.blogpost.default;
+
+    const {
+      html,
+      title,
+      attributes: { thumbnail },
+    } = this.props.blogpost.default;
+
     return (
       <article>
         <h1>{title}</h1>
+        <img src={thumbnail} />
         <div dangerouslySetInnerHTML={{ __html: html }} />
       </article>
     );
