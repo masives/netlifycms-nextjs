03912afea092e6f0ea15d58b44115a043dedbd64
Jozef Piecyk <jozef.piecyk@10clouds.com>
2019-09-05 15:30:40 +0200

    change api endpoint

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,13 @@ import React, { Component } from 'react';
 import Link from 'next/link';
 import axios from 'axios';
 import content from '../content/home.md';
-
+// console.log(process.env);
 export default class Home extends Component {
   static async getInitialProps({}) {
-    const postsList = await axios.get('http://localhost:3000/api/posts');
+    const isDev = process.env.NODE_ENV === 'development';
+    const apiHref = isDev ? 'http://localhost:3000/api/posts' : `/api/posts`;
+
+    const postsList = await axios.get(apiHref);
     return { postsList: postsList.data };
   }
   render() {
