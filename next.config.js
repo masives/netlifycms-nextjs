093208b1153e7ada6a90f4f0f26c7feb30e14a93
Jozef Piecyk <jozef.piecyk@10clouds.com>
2019-09-05 12:56:52 +0200

    add new handling for slugs

--- next.config.js
+++ next.config.js
@@ -6,4 +6,35 @@ module.exports = {
     });
     return configuration;
   },
+  async exportPathMap(defaultPathMap) {
+    const fs = require('fs');
+    const blogPostsFolder = './content/blogPosts';
+
+    const blogList = fs
+      .readdirSync(blogPostsFolder)
+      .map(blogName => {
+        const trimmedName = blogName.substring(0, blogName.length - 3);
+        return {
+          [`/posts/${trimmedName}`]: {
+            page: '/posts',
+            query: {
+              slug: trimmedName,
+            },
+          },
+        };
+      })
+      .reduce((acc, curr) => {
+        console.log({ acc, curr });
+        return { ...acc, ...curr };
+      }, {});
+    // .forEach(dups => console.log({ dups }));
+    console.log({ blogList });
+    console.log({ ...defaultPathMap, ...blogList });
+    return {
+      '/index': { page: '/index' },
+      // '/posts/[slug]': { page: '/posts/[slug]' },
+      '/': { page: '/' },
+      ...blogList,
+    };
+  },
 };
---
+++ pages/posts/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+// import { useRouter } from 'next/router';
+
+class Post extends Component {
+  static async getInitialProps({ query }) {
+    // pid = 'hello-nextjs'
+    const { slug } = query;
+    const blogpost = await import(`../../content/blogPosts/${slug}.md`).catch(error => null);
+
+    return { blogpost };
+  }
+  render() {
+    console.log('leci cokolwiek?');
+    //   empty post
+    if (!this.props.blogpost) return <div>not found</div>;
+    // const router = useRouter();
+    const { html, title } = this.props.blogpost.default;
+    return (
+      <article>
+        <h1>{title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </article>
+    );
+  }
+}
+
+export default Post;
