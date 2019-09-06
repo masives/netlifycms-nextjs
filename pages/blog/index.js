2e32d505456cb5da6cf6aeb66549a45530921ed1
Jozef Piecyk <jozef.piecyk@10clouds.com>
2019-09-06 13:03:05 +0200

    move blog away from home

---
+++ pages/blog/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+import Link from 'next/link';
+
+const BLOG_POSTS_PATH = '../../content/blogPosts';
+
+const importBlogPosts = async () => {
+  // https://medium.com/@shawnstern/importing-multiple-markdown-files-into-a-react-component-with-webpack-7548559fce6f
+  // second flag in require.context function is if subdirectories should be searched
+  const markdownFiles = require
+    .context('../../content/blogPosts', false, /\.md$/)
+    .keys()
+    .map(relativePath => relativePath.substring(2));
+  return Promise.all(
+    markdownFiles.map(async path => {
+      const markdown = await import(`../../content/blogPosts/${path}`);
+      return { ...markdown, path: path.substring(0, path.length - 3) };
+    })
+  );
+};
+
+export default class Blog extends Component {
+  static async getInitialProps() {
+    const postsList = await importBlogPosts();
+
+    return { postsList };
+  }
+  render() {
+    const { postsList } = this.props;
+    return (
+      <div className="blog-list">
+        {postsList.map(post => {
+          return (
+            <Link href={`/posts/${post.path}`}>
+              <a>
+                <img src={post.attributes.thumbnail} />
+                <h2>{post.attributes.title}</h2>
+              </a>
+            </Link>
+          );
+        })}
+        <style jsx>{`
+          .blog-list {
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            grid-gap: 20px;
+          }
+          .blog-list a {
+            display: block;
+            text-align: center;
+          }
+          .blog-list img {
+            max-width: 100%;
+
+            height: auto;
+          }
+        `}</style>
+      </div>
+    );
+  }
+}
