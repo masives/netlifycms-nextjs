65755335ac0c523e35c778b2684fdfc0ef9d7fff
Jozef Piecyk <jozef.piecyk@10clouds.com>
2019-09-06 13:02:55 +0200

    update home page to use md file content

--- content/home.md
+++ content/home.md
@@ -1,14 +1,7 @@
 ---
-title: Awesome kitties
+title: Home
 date: 2019-03-17T19:31:20.591Z
-cats:
-  - description: 'Maru is a Scottish Fold from Japan, and he loves boxes.'
-    name: Maru (まる)
-  - description: Lil Bub is an American celebrity cat known for her unique appearance.
-    name: Lil Bub
-  - description: 'Grumpy cat is an American celebrity cat known for her grumpy appearance. '
-    name: Grumpy cat (Tardar Sauce)
 ---
-Welcome to my awesome page about cats of the internet. 
+### Sample netflifycms in next.js page
 
-This page is built with NextJS, and content is managed in Netlify CMS
+Look at the repository to check it out [link](https://github.com/masives/netlifycms-nextjs/tree/master/content/blogPosts)
--- pages/index.js
+++ pages/index.js
@@ -3,34 +3,19 @@ import React, { Component } from 'react';
 import Link from 'next/link';
 import content from '../content/home.md';
 
-const importBlogPosts = async () => {
-  // https://medium.com/@shawnstern/importing-multiple-markdown-files-into-a-react-component-with-webpack-7548559fce6f
-  // second flag in require.context function is if subdirectories should be searched
-  const markdownFiles = require
-    .context('../content/blogPosts', false, /\.md$/)
-    .keys()
-    .map(relativePath => relativePath.substring(2));
-  return Promise.all(markdownFiles.map(path => import(`../content/blogPosts/${path}`)));
-};
-
 export default class Home extends Component {
-  static async getInitialProps() {
-    const postsList = await importBlogPosts();
-
-    return { postsList };
-  }
   render() {
-    const { postsList } = this.props;
+    const { attributes, html } = content;
     return (
       <React.Fragment>
-        {postsList.map(post => {
-          console.log({ post });
-          return (
-            <Link href={`/posts/${post.slug}`}>
-              <a>{post.attributes.title}</a>
-            </Link>
-          );
-        })}
+        <h1>{attributes.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+        <style jsx>{`
+          h1,
+          div {
+            text-align: center;
+          }
+        `}</style>
       </React.Fragment>
     );
   }
